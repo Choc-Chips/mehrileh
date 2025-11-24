93787e1911029ca89c0de7d70b00df5665382d30
Choc-Chips <anarchotuga1@gmail.com>
2025-11-24 16:11:47 +0000

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import dictionary from "./dictionary.json";
 
 function Dictionary({ baseWords, language }) {
-
   const [input, setInput] = useState("");
   const words = baseWords.filter((word) => word.english.match(`${input}`));
   const [chosen, setChosen] = useState(null);
@@ -16,51 +15,63 @@ function Dictionary({ baseWords, language }) {
     setChosen(words.find((w) => w.english === clickedWord));
   }
 
-  return(
+  return (
     <div className="cards">
-        <div className='card'>
-          <form action="">
-            <input
-              type="text"
-              name="word"
-              value={input}
-              onChange={handleInput}
-            />
-          </form>
-          <div>
-            {words.map((word) => (
-              <p key={word.id} onClick={() => handleClick(word.english)} className={`word ${word === chosen?'chosen':null}`}>
-                {language==="english" ? word.english : word.mehrileh}
-              </p>
-            ))}
-          </div>
-        </div>
-        <div className="separation"></div>
-        <div className='card'>
-          {chosen ? (
-            <>
-              <h3>{chosen.mehrileh}</h3>
-              <p>/{chosen.pronunciation}/</p>
-              <p>{chosen.english}</p>
-            </>
-          ): <></>}
+      <div className="card">
+        <form action="">
+          <input type="text" name="word" value={input} onChange={handleInput} placeholder="Search a word"/>
+        </form>
+        <div>
+          {words.map((word) => (
+            <p
+              key={word.id}
+              onClick={() => handleClick(word.english)}
+              className={`word ${word === chosen ? "chosen" : null}`}
+            >
+              {language === "english" ? word.english : word.mehrileh}
+            </p>
+          ))}
         </div>
       </div>
-  )
+      <div className="separation"></div>
+      <div className="card">
+        {chosen ? (
+          <>
+            <h3>{chosen.mehrileh}</h3>
+            <p>/{chosen.pronunciation}/</p>
+            <p>{chosen.english}</p>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 }
 
 function App() {
-
-  const [baseWords] = useState(dictionary.sort((a, b) => a.english > b.english));
-  const [baseWordsDifSort] = useState(baseWords.sort((a, b) => a.mehrileh > b.mehrileh));
+  // Sort dictionary alphabetically
+  const [baseWords] = useState(
+    dictionary.sort((a, b) => a.english > b.english)
+  );
+  // Sort dictionary for mehrileh
+  const [baseWordsDifSort] = useState(
+    baseWords.sort((a, b) => a.mehrileh > b.mehrileh)
+  );
 
   return (
     <>
       <h1>Mehrileh Dictionary</h1>
-      <h2>English to Mehrileh</h2>
-      <Dictionary baseWords={baseWords} language="english" />
-      <h2>Mehrileh to English</h2>
-      <Dictionary baseWords={baseWordsDifSort} language="mehrileh" />
+      <div className="sections">
+        <div className="section">
+          <h2>English to Mehrileh</h2>
+          <Dictionary baseWords={baseWords} language="english" />
+        </div>
+        <div className="section">
+          <h2>Mehrileh to English</h2>
+          <Dictionary baseWords={baseWordsDifSort} language="mehrileh" />
+        </div>
+      </div>
     </>
   );
 }
