154d1c54f974f4664823eb05452570ec8f8b615e
Choc-Chips <anarchotuga1@gmail.com>
2025-11-24 14:06:23 +0000

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,51 @@
-import { useState } from 'react'
-import './App.css'
-import dictionary from './dictionary.json'
+import { useState } from "react";
+import "./App.css";
+import dictionary from "./dictionary.json";
 
 function App() {
-
   const [baseWords] = useState(dictionary);
   const [input, setInput] = useState("");
-  const words = baseWords.filter(word => word.english.match(`${input}`));
+  const words = baseWords.filter((word) => word.english.match(`${input}`));
   const [chosen, setChosen] = useState(null);
 
   function handleInput(e) {
     setInput(e.target.value);
   }
-  
+
   function handleClick(e) {
     const tempChosen = e.target.textContent;
-    setChosen(words.find(w => w.english === tempChosen));
+    setChosen(words.find((w) => w.english === tempChosen));
   }
 
   return (
     <>
       <h1>Mehrileh Dictionary</h1>
-      <div>
-        <form action="">
-          <input type="text" name='word' value={ input } onChange={ handleInput }/>
-        </form>
-        <div>
-          { words.map( word => (
-            <h2 key={ word.id } onClick={ handleClick }>{word.english}</h2>
-          ))}
+      <div className="cards">
+        <div className='card'>
+          <form action="">
+            <input
+              type="text"
+              name="word"
+              value={input}
+              onChange={handleInput}
+            />
+          </form>
+          <div>
+            {words.map((word) => (
+              <p  key={word.id} onClick={handleClick} className={`word ${word === chosen?'chosen':null}`}>
+                {word.english}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="separation"></div>
+        <div className='card'>
+          <h2>{chosen?.english}</h2>
+          <p>{chosen?.mehrileh}</p>
         </div>
       </div>
-      <div>
-          <h2>{ chosen?.english }</h2>
-          <p>{ chosen?.mehrileh }</p>
-      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
