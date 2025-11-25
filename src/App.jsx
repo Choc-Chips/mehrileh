e59c5d0f3a859ee88521e463d9031b0a6c984837
Choc-Chips <anarchotuga1@gmail.com>
2025-11-25 14:53:53 +0000

    fix mehrileh section

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,61 @@ import { useState } from "react";
 import "./App.css";
 import dictionary from "./dictionary.json";
 
+
+function Word({ word, language, selected, setSelected }) {
+
+  function handleClick() {
+    setSelected(word);
+  } 
+
+  return (
+    <p
+      key={word.id}
+      onClick={() => handleClick(word.english)}
+      className={`word ${word === selected ? "selected" : null}`}
+    >
+      {language === "english" ? word.english : word.mehrileh}
+    </p>
+  );
+}
+
 function Dictionary({ baseWords, language }) {
   const [input, setInput] = useState("");
-  const words = baseWords.filter((word) => word.english.match(`${input}`));
-  const [chosen, setChosen] = useState(null);
+  let words;
+  if (language === "english") words = baseWords.filter((word) => word.english.match(`${input}`));
+  if (language === "mehrileh") words = baseWords.filter((word) => word.mehrileh.match(`${input}`));
+  const [selected, setSelected] = useState(null);
 
   function handleInput(e) {
     setInput(e.target.value);
-  }
-
-  function handleClick(clickedWord) {
-    setChosen(words.find((w) => w.english === clickedWord));
   }
 
   return (
     <div className="cards">
       <div className="card">
         <form action="">
-          <input type="text" name="word" value={input} onChange={handleInput} placeholder="Search a word"/>
+          <input
+            type="text"
+            name="word"
+            value={input}
+            onChange={handleInput}
+            placeholder="Search a word"
+          />
         </form>
         <div>
           {words.map((word) => (
-            <p
-              key={word.id}
-              onClick={() => handleClick(word.english)}
-              className={`word ${word === chosen ? "chosen" : null}`}
-            >
-              {language === "english" ? word.english : word.mehrileh}
-            </p>
+           <Word word={word} language={language} selected={selected} setSelected={setSelected} />
           ))}
         </div>
       </div>
       <div className="separation"></div>
       <div className="card">
-        {chosen ? (
+        {selected ? (
           <>
-            <h3>{chosen.mehrileh}</h3>
-            <p>/{chosen.pronunciation}/</p>
-            <p>{chosen.english}</p>
+            <h3>{selected.mehrileh}</h3>
+            <p className="pronunciation">/{selected.pronunciation}/</p>
+            <p className="class">{selected.class}</p>
+            <p>{selected.englishFull}</p>
           </>
         ) : (
           <></>
@@ -51,15 +68,26 @@ function Dictionary({ baseWords, language }) {
 
 function App() {
   // Sort dictionary alphabetically
-  const [baseWords] = useState(
-    [... dictionary.sort((a, b) => a.english > b.english)]
-  );
+  const [baseWords] = useState([
+    ...dictionary.sort((a, b) => a.english > b.english),
+  ]);
+
+  // Remove redudant instances where the full english translation is the same
+  function removeDuplicatesByEnglishFull(dictionary) {
+    let temp = [...dictionary.sort((a, b) => a.mehrileh > b.mehrileh)];
+    const seen = new Map();
+    return temp.filter((item) => {
+      if (!seen.has(item.englishFull)) {
+        seen.set(item.englishFull, true);
+        return true;
+      }
+    });
+  }
 
   // Sort dictionary for mehrileh
-  const [baseWordsDifSort] = useState(
-    [... dictionary.sort((a, b) => a.mehrileh > b.mehrileh)]
+  const [baseWordsMehrileh] = useState(
+    removeDuplicatesByEnglishFull(dictionary)
   );
-
 
   return (
     <>
@@ -71,7 +99,7 @@ function App() {
         </div>
         <div className="section">
           <h2>Mehrileh to English</h2>
-          <Dictionary baseWords={baseWordsDifSort} language="mehrileh" />
+          <Dictionary baseWords={baseWordsMehrileh} language="mehrileh" />
         </div>
       </div>
     </>
